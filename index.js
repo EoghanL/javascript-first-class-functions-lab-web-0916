fe08aa809e3418b7f0ceed4cf50a58512ea2c2b6
github username <github email address>
2016-11-05 12:19:43 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,18 @@
+var doubler = createMultiplier(2)
+var tripler = createMultiplier(3)
+var doublerWithBind = multiplier.bind(null, 2)
+var triplerWithBind = multiplier.bind(null, 3)
+
+function countdown(callback){
+  return window.setTimeout(callback, 2000);
+}
+
+function createMultiplier(num1){
+  return function(num2){
+    return num1 * num2
+  }
+}
+
+function multiplier(num1, num2){
+  return num1 * num2
+}
